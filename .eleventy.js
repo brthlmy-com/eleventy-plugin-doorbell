501fe768197764509a49e9840e2aa1ce4794a228
Eric Bartholemy <eric.bartholemy@gmail.com>
2023-05-05 21:36:28 +0200

    adds headless brthlmy doorbell

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,62 @@
+const UglifyJS = require('uglify-js');
 // Example use for the demo plugin:
-// {{ 'Steph' | hello | safe }}
+// Uses transform to hook into head, and body tags.
 
-module.exports = (eleventyConfig, options) => {
-  // Define defaults for your plugin config
-  const defaults = {
-    htmlTag: "h2",
+// Define defaults for your plugin config
+const defaults = {
+  api: 'https://your-domain.com/.netlify/functions/doorbell',
+};
+
+module.exports = (eleventyConfig, _options) => {
+  // Combine defaults with user defined options
+  const options = {
+    ...defaults,
+    ..._options,
   };
 
-  // You can create more than filters as a plugin, but here's an example
-  eleventyConfig.addFilter("hello", (name) => {
-    // Combine defaults with user defined options
-    const { htmlTag } = {
-      ...defaults,
-      ...options,
-    };
+  const script = `
+      const src = '${options.api}';
 
-    return `<${htmlTag}>Hello, ${name}!</${htmlTag}>`;
+      const { pathname, search, hash } = window.location;
+
+      const params = new URLSearchParams();
+      params.append('pathname', pathname);
+      params.append('search', search);
+      params.append('hash', hash);
+
+      const img = document.createElement('img');
+      img.src = [src, params].join('?');
+    `;
+
+  const noscript = `
+    <noscript>
+      <img
+        src="${options.api}?noscript"
+        alt="doorbell"
+        width="0"
+        height="0"
+        hidden
+        decoding="async"
+        loading="eager"
+      />
+    </noscript>`;
+
+  eleventyConfig.addTransform('doorbell', (content, outputPath) => {
+    if (outputPath && outputPath.endsWith('.html')) {
+      return [
+        content.split('</head>').join(`</head>${noscript}`),
+        content
+          .split('</body>')
+          .join(
+            `${
+              UglifyJS.minify(script, {
+                compress: true,
+                mangle: {toplevel: true},
+              }).code
+            }</body>`,
+          ),
+      ].join('');
+    }
+    return content;
   });
 };
